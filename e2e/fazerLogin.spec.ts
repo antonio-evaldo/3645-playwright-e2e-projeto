accc594b5218282d1d874a414201f63ca1cf77bb
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-19 17:32:23 -0300

    Testando login inválido

--- e2e/fazerLogin.spec.ts
+++ e2e/fazerLogin.spec.ts
@@ -11,4 +11,18 @@ test.describe('Login', () => {
     await page.getByTestId('botao-acessar-conta').click();
     await expect(page).toHaveURL('/home');
   });
+
+  test('Não deve conseguir fazer login com email inválido', async ({ page }) => {
+    await page.goto('/');
+    await page.getByTestId('botao-login').click();
+    await expect(page).toHaveURL('/auth/login');
+    
+    await page.getByTestId('input-email').fill('antonio.evaldo@errado.com');
+    await page.getByTestId('input-senha').fill('123456');
+    await page.getByTestId('botao-acessar-conta').click();
+
+    const mensagemErro = page.getByText('Você não está autorizado a acessar este recurso');
+
+    await expect(mensagemErro).toBeVisible();
+  });
 });
