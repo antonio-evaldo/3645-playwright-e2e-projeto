b7779725e286d990bb27e5ac4ee84c626103b0b3
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-19 17:16:53 -0300

    Vídeo 3.2: Completando teste de login

--- e2e/fazerLogin.spec.ts
+++ e2e/fazerLogin.spec.ts
@@ -4,5 +4,11 @@ test.describe('Login', () => {
   test('Deve conseguir fazer login com email e senha válidos', async ({ page }) => {
     await page.goto('/');
     await page.getByTestId('botao-login').click();
+    await expect(page).toHaveURL('/auth/login');
+    
+    await page.getByTestId('input-email').fill('antonio.evaldo@alura.com');
+    await page.getByTestId('input-senha').fill('123456');
+    await page.getByTestId('botao-acessar-conta').click();
+    await expect(page).toHaveURL('/home');
   });
 });
