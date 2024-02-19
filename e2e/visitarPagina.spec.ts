1abd99baa599e9463192b997017fb5d63b98f067
Antônio Evaldo <antonio.evaldo349@gmail.com>
2024-02-19 16:00:51 -0300

    Configurando baseURL

--- e2e/visitarPagina.spec.ts
+++ e2e/visitarPagina.spec.ts
@@ -2,15 +2,11 @@ import test, { expect } from "@playwright/test";
 
 test.describe("Página inicial", () => {
   test("Deve conter os elementos principais", async ({ page }) => {
-    await page.goto("http://localhost:4200");
+    await page.goto("/");
     await expect(page).toHaveTitle("Jornada Milhas");
 
     const tituloPassagens = page.getByRole('heading', { name: 'Passagens' });
-    const tituloPromocoes = page.getByRole('heading', { name: 'Promoções' });
-    const tituloDepoimentos = page.getByRole('heading', { name: 'Depoimentos' });
     
     await expect(tituloPassagens).toBeVisible();
-    await expect(tituloPromocoes).toBeVisible();
-    await expect(tituloDepoimentos).toBeVisible();
   });
 });
